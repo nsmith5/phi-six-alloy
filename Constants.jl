a3e1f8bf4f51013b8e68e90780763e8fa3b183a0
Administrator <nathan.frederick.smith@gmail.com>
2015-07-23 14:52:03 -0400

    Initial Commit

---
+++ Constants.jl
@@ -0,0 +1,14 @@
+# ----------------------------
+#  Constants for simulation
+# ----------------------------
+
+module Constants
+
+export W, r, u, v
+
+W = 1.0
+r = 1.0
+u = -1.0
+v = 1.0
+
+end
---
+++ Methods.jl
@@ -0,0 +1,13 @@
+# ---------------------------------
+#  Methods required for simulation
+# ---------------------------------
+
+module Methods
+
+using Constants
+
+export x
+
+x = 10
+
+end
